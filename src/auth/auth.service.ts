19733b81a7a524ebda7d2aee883bf512fb003ea4
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-09-23 10:42:18 +0600

    all service function declared

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { LoginDto } from './dto';
 import { CreateAuthInput } from './dto/create-auth.input';
-import { UpdateAuthInput } from './dto/update-auth.input';
+import { LogoutDto } from './dto/logout.dto';
 
 @Injectable()
 export class AuthService {
-    create(createAuthInput: CreateAuthInput) {
-        return 'This action adds a new auth';
+    constructor(private readonly userService: UserService) {}
+
+    async signup(dto: CreateAuthInput) {
+        return this.userService.create(dto);
     }
 
-    findAll() {
-        return `This action returns all auth`;
+    async login(dto: LoginDto) {
+        return this.userService.login(dto);
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} auth`;
+    async logout(dto: LogoutDto) {
+        return this.userService.logout(dto);
     }
 
-    update(id: number, updateAuthInput: UpdateAuthInput) {
-        return `This action updates a #${id} auth`;
+    async getMe(id: string) {
+        return this.userService.findOne(id);
     }
 
-    remove(id: number) {
-        return `This action removes a #${id} auth`;
+    async refreshToken(dto: any) {
+        return this.userService.refreshToken(dto);
+    }
+
+    async validateUser(email: string, pass: string): Promise<any> {
+        const user = await this.userService.findOneByEmail(email);
+        if (user && user.password === pass) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
     }
 }
