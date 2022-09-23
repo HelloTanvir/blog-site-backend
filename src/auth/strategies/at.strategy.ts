1bdce917089ab60560b95ad8a6e199188d2cbd8a
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-09-23 15:00:19 +0600

    access token and refresh token strategy added

---
+++ src/auth/strategies/at.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserService } from '../../user/user.service';
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
+    constructor(private config: ConfigService, private readonly userService: UserService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: config.get('AT_SECRET_KEY'),
+        });
+    }
+
+    async validate(payload: any) {
+        const user = await this.userService.findOne(payload['userId']);
+
+        if (!user || !user.refreshToken) {
+            throw new UnauthorizedException();
+        }
+
+        return user;
+    }
+}
