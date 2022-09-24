38800295c65d8355a63863d4811f6228863d7005
HelloTanvir <tanvirhossain.butex@gmail.com>
2022-09-24 10:39:53 +0600

    removed non used function argument

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -1,29 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PostCreateDto, PostGetDto } from './dto';
 import { PostService } from './post.service';
 import { Post } from './schema';
 
-@Resolver((of) => Post)
+@Resolver(() => Post)
 export class PostResolver {
     constructor(private postService: PostService) {}
 
-    @Mutation((returns) => Post)
+    @Mutation(() => Post)
     createPost(@Args('createPostInput') dto: PostCreateDto): Promise<Post> {
         return this.postService.create(dto);
     }
 
-    @Query((returns) => [Post])
+    @Query(() => [Post])
     posts(@Args({ name: 'getPostInput' }) dto: PostGetDto): Promise<Post[]> {
         return this.postService.findAll(dto);
     }
 
-    @Query((returns) => Post)
+    @Query(() => Post)
     post(@Args({ name: 'id', type: () => ID }) postId: string): Promise<Post> {
         return this.postService.findOne(postId);
     }
 
-    @Mutation((returns) => Post)
+    @Mutation(() => Post)
     deletePost(@Args({ name: 'id', type: () => ID }) postId: string): Promise<Post> {
         return this.postService.delete(postId);
     }
